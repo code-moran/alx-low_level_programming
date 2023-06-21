ad859085c1f950b2778364eb08d36b06220ef42e
Titus Njiru <titokym96@gmail.com>
2023-06-21 11:28:02 -0700

    correct betty style formatting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,36 +5,36 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0'); /* Print the first digit */
+		_putchar(',');
 
-    for (row = 0; row <= 9; row++)
-    {
-        _putchar('0'); /* Print the first digit */
-        _putchar(',');
+		for (column = 1; column <= 9; column++)
+		{
+			product = row * column;
 
-        for (column = 1; column <= 9; column++)
-        {
-            product = row * column;
+			_putchar(' ');
 
-            _putchar(' ');
+			if (product <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((product / 10) + '0'); /* Print the tens digit */
+			}
+			
+			_putchar((product % 10) + '0'); /* Print the ones digit */
 
-            if (product <= 9)
-            {
-                _putchar(' '); /* Print leading space for single-digit numbers */
-            }
-            else
-            {
-                _putchar((product / 10) + '0'); /* Print the tens digit */
-            }
+			if (column < 9)
+			{
+				_putchar(',');
+			}
+		}
 
-            _putchar((product % 10) + '0'); /* Print the ones digit */
-
-            if (column < 9)
-            {
-                _putchar(',');
-            }
-        }
-
-        _putchar('\n');
-    }
+		_putchar('\n');
+	}
 }
+
