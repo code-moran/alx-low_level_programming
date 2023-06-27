9f37a441d0e810c92d422e104fdfaf34036060e5
Titus Njiru <titokym96@gmail.com>
2023-06-27 08:04:53 -0700

    fix errors

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,12 +7,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	/* Store the value of first integer in a temporary variable */
 	int temp = *a;
 
-	/* Assign the value of second integer to the first integer */
 	*a = *b;
 
-	/* Assign the value of temporary variable to the second integer */
 	*b = temp;
 }
