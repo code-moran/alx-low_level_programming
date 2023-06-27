5ddd9599ecfc9a186082e826dfcbb89e33052642
Titus Njiru <titokym96@gmail.com>
2023-06-27 08:44:11 -0700

    fix alignment error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    int length, i;
-    char temp;
+	int length, i;
+	char temp;
 
-    /* Calculate the length of the string */
-    length = 0;
-    while (s[length] != '\0')
-        length++;
+	/* Calculate the length of the string */
+	length = 0;
+	while (s[length] != '\0')
+		length++;
 
-    /* Swap characters from the start and end of the string */
-    for (i = 0; i < length / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[length - 1 - i];
-        s[length - 1 - i] = temp;
-    }
+	/* Swap characters from the start and end of the string */
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
 }
