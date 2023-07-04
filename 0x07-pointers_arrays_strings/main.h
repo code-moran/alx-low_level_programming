bf3bd3020a5e0aa7112c89e714ac23bcc55b6e07
Titus Njiru <titokym96@gmail.com>
2023-07-04 02:34:20 -0700

    add a function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 /* Function prototype */
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
