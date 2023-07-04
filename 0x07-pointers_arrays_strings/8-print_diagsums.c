51bab9d1d19297adf1a4bafdd15f90de76094482
Titus Njiru <titokym96@gmail.com>
2023-07-04 13:17:46 -0700

    format code with tabs

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,21 +11,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int sum1 = 0;   /* Sum of the main diagonal */
-    int sum2 = 0;   /* Sum of the secondary diagonal */
-    int i;
+	int sum1 = 0;	/* Sum of the main diagonal */
+	int sum2 = 0;	/* Sum of the secondary diagonal */
+	int i, j;
 
-    /* Calculate the sums of the diagonals */
-    for (i = 0; i < size; i++)
-    {
-        /* Calculate the sum of the main diagonal elements */
-        sum1 += a[i * size + i];
+	/* Calculate the sums of the diagonals */
+	for (i = 0; i < size; i++)
+	{
+		/* Calculate the sum of the main diagonal elements */
+		sum1 += a[i * size + i];
 
-        /* Calculate the sum of the secondary diagonal elements */
-        sum2 += a[i * size + (size - 1 - i)];
-    }
+		/* Calculate the sum of the secondary diagonal elements */
+		sum2 += a[i * size + (size - 1 - i)];
+	}
 
-    /* Print the sums of the diagonals */
-    printf("%d, ", sum1);
-    printf("%d\n", sum2);
+	/* Print the sums of the diagonals */
+	printf("%d ", sum1);
+	printf("%d\n", sum2);
 }
