0f8bf472f5cbe9308282876fb7f28a3dfb7a1850
Titus Njiru <titokym96@gmail.com>
2023-07-05 12:39:27 -0700

    add a function that returns the length of a string.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _puts_recursion - Prints a string followed by a new line.
- * @s: The string to be printed.
- */
+/* function prototpes  */
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
