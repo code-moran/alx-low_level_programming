4156d500b3e2e1280b105e4cba7e1576537065d6
Titus Njiru <titokym96@gmail.com>
2023-07-05 12:53:21 -0700

    add  a function that returns the factorial of a given number

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string for which to determine the length.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0') {
+		return (0);	/* Base case: end of string is reached */
+	}
+
+	return (1 + _strlen_recursion(s + 1));	/* Recursively call the function with the next character */
+}
