294a3cda7d7856ea83d40b257991215145823300
Titus Njiru <titokym96@gmail.com>
2023-07-24 06:29:24 -0700

    format code

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  */
 void print_numbers(void)
 {
-    char digit;
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        _putchar(digit);
-    }
-    _putchar('\n');
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		_putchar(digit);
+	}
+	_putchar('\n');
 }
 
