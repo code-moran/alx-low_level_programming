cd314b6d5c9456a35553c7ef5a4377aee3011d43
Titus Njiru <titokym96@gmail.com>
2023-07-27 12:01:10 -0700

    add  a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello_msg db 'Hello, Holberton', 0
+    newline db 10, 0   ; New line character '\n'
 
 section .text
     global main
@@ -11,8 +12,12 @@ main:
     push rbp
     mov rbp, rsp
 
-    ; Call the printf function
+    ; Call the printf function to print the message
     mov rdi, hello_msg ; Set the format string pointer
+    call printf       ; Call the printf function
+
+    ; Call the printf function to print the new line
+    mov rdi, newline   ; Set the format string pointer to new line character
     call printf       ; Call the printf function
 
     ; Clean up the stack frame and exit the program
