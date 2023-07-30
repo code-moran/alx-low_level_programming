126adc9297faac9380e7a3028eeb90c8b3792e4d
Titus Njiru <titokym96@gmail.com>
2023-07-30 11:41:39 -0700

    include guard header file dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - A structure representing information about a dog.
  * @name: The name of the dog.
@@ -5,10 +8,12 @@
  * @owner: The name of the dog's owner.
  *
  * Description: This structure holds information about a dog,
- * such as its name, age, and owner's name.
+ *              such as its name, age, and owner's name.
  */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
