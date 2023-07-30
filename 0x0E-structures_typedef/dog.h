8074be19ee586fc5141b419ee45ef10b61014a24
Titus Njiru <titokym96@gmail.com>
2023-07-30 11:23:15 -0700

    add a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - A structure representing information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the dog's owner.
+ *
+ * Description: This structure holds information about a dog,
+ * such as its name, age, and owner's name.
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
