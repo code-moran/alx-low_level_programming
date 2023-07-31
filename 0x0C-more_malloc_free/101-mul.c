6a3fb195aba3075d75b39e9a98d9d51b9af05af9
Titus Njiru <titokym96@gmail.com>
2023-07-31 00:14:00 -0700

    add a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,89 +3,105 @@
 #include <string.h>
 
 /**
- * multiply_strings - Multiply two positive numbers given as strings.
- * @num1: First number as a string.
- * @num2: Second number as a string.
+ * multiply - Multiplies two positive numbers represented as strings.
+ * @num1: The first number as a string.
+ * @num2: The second number as a string.
  *
- * Return: Pointer to the result of multiplication as a string,
- *         or NULL on failure.
+ * Return: The product as a string, or NULL if there's an error.
  */
-char *multiply_strings(const char *num1, const char *num2)
-{
+char *multiply(const char *num1, const char *num2) {
 	int len1 = strlen(num1);
 	int len2 = strlen(num2);
-	int len3 = len1 + len2;
-	int i, j;
-	char *result;
-	char *multiplied;
+	int len = len1 + len2;
+	int *result = calloc(len, sizeof(int));
+	int i;
+	int index = 0;
+	char *strResult;
+	int j;
 
-	result = calloc(len3 + 1, sizeof(char));
-
-	if (result == NULL)
-	{
-		perror("Memory allocation failed");
-		return NULL;
+	if (!result) {
+		return (NULL);	/* Memory allocation failed */
 	}
 
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		int carry = 0;
-		int n1 = num1[i] - '0';
+	/* Perform multiplication digit by digit */
+	for (i = len1 - 1; i >= 0; i--) {
+		for (j = len2 - 1; j >= 0; j--) {
+			int digit1 = num1[i] - '0';
+			int digit2 = num2[j] - '0';
+			int product = digit1 * digit2;
 
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			int n2 = num2[j] - '0';
-			int sum = (result[i + j + 1] - '0') + (n1 * n2) + carry;
-			result[i + j + 1] = (sum % 10) + '0';
-			carry = sum / 10;
+			int sum = result[i + j + 1] + product;
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
 		}
-
-		result[i] += carry;
 	}
 
-	/* Remove leading zeros */
-	i = 0;
-	while (result[i] == '0' && i < len3 - 1)
-	{
-		i++;
+	/* Convert the result to a string */
+	strResult = malloc((len + 1) * sizeof(char));
+	if (!strResult) {
+		free(result);
+		return (NULL);	/* Memory allocation failed */
 	}
 
-	multiplied = strdup(result + i);
+	while (index < len && result[index] == 0) {
+		index++;	/* Skip leading zeros */
+	}
+
+	if (index == len) {
+		strResult[0] = '0';
+		strResult[1] = '\0';
+	} else {
+		for (i = index; i < len; i++) {
+			strResult[i - index] = result[i] + '0';
+		}
+		strResult[len - index] = '\0';
+	}
 
 	free(result);
-	return (multiplied);
+	return (strResult);
 }
 
 /**
  * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of pointers to command-line arguments.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the arguments.
  *
- * Return: 0 on success, 1 on failure.
+ * Return: Always 0 on success, 98 on error.
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char *num1 = argv[1];
-        char *num2 = argv[2];
+	char *num2 = argv[2];
+	int i;
+	char *result;
 
-        char *result = multiply_strings(num1, num2);
-
-	if (argc != 3)
-	{
-		printf("Usage: %s num1 num2\n", argv[0]);
-		return (1);
+	if (argc != 3) {
+		printf("Error\n");
+		return (98);
 	}
 
-	if (result != NULL)
-	{
-		printf("%s\n", result);
-		free(result);
+	/* Check if num1 and num2 are composed of digits */
+	for (i = 0; num1[i] != '\0'; i++) {
+		if (num1[i] < '0' || num1[i] > '9') {
+			printf("Error\n");
+			return (98);
+		}
 	}
-	else
-	{
-		printf("Error: Memory allocation failed\n");
-		return (1);
+
+	for (i = 0; num2[i] != '\0'; i++) {
+		if (num2[i] < '0' || num2[i] > '9') {
+			printf("Error\n");
+			return (98);
+		}
 	}
+
+	result = multiply(num1, num2);
+	if (result == NULL) {
+		printf("Error\n");
+		return (98);
+	}
+
+	printf("%s\n", result);
+	free(result);
 
 	return (0);
 }
