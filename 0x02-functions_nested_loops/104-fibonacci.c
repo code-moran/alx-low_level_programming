818a6c5d5cde3790084e0917c006258c8e42cd56
Titus Njiru <titokym96@gmail.com>
2023-08-02 10:12:11 -0700

    fix big numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,69 @@
 #include <stdio.h>
 
+#define MAX_DIGITS 1000
+
+/**
+ * add_arrays - Adds two arrays of digits and stores the result in res.
+ *
+ * @a: The first array of digits.
+ * @b: The second array of digits.
+ * @res: The array to store the result.
+ */
+void add_arrays(int a[], int b[], int res[])
+{
+	int carry = 0;
+	int i;
+	int sum;
+
+	for (i = MAX_DIGITS - 1; i >= 0; i--)
+	{
+		sum = a[i] + b[i] + carry;
+		res[i] = sum % 10;
+		carry = sum / 10;
+	}
+}
+
+/**
+ * print_array - Prints an array of digits.
+ *
+ * @arr: The array of digits to print.
+ */
+void print_array(int arr[])
+{
+	int i = 0;
+	while (i < MAX_DIGITS - 1 && arr[i] == 0)
+		i++;
+
+	for (; i < MAX_DIGITS; i++)
+		printf("%d", arr[i]);
+}
+
 /**
  * main - Finds and prints the first 98 Fibonacci numbers.
  *
- * Return: (0) on successful completion.
+ * Return: 0 on successful completion.
  */
 int main(void)
 {
-	int i;
-	unsigned long a = 1, b = 2, c;
+	int i, j;
+	int fib1[MAX_DIGITS] = {0};
+	int fib2[MAX_DIGITS] = {0};
+	int result[MAX_DIGITS] = {0};
 
-	printf("%lu, %lu, ", a, b);
+	fib2[MAX_DIGITS - 1] = 1;
 
-	for (i = 2; i < 98; i++)
+	printf("1");
+	for (i = 2; i <= 98; i++)
 	{
-		c = a + b;
-		printf("%lu", c);
+		add_arrays(fib1, fib2, result);
+		printf(", ");
+		print_array(result);
 
-		if (i < 97)
-			printf(", ");
-
-		a = b;
-		b = c;
+		for (j = 0; j < MAX_DIGITS; j++)
+		{
+			fib1[j] = fib2[j];
+			fib2[j] = result[j];
+		}
 	}
 
 	printf("\n");
