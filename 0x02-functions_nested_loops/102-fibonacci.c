5c5ad0854ed8dfd9d6992266dae67df2b6e2c55c
Titus Njiru <titokym96@gmail.com>
2023-08-02 09:08:52 -0700

    fix errors

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 	int fib[50];
+	int i;
+
 	fib[0] = 1;
 	fib[1] = 2;
-	int i;
 
 	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
-	for (int i = 0; i < 49; i++)
+	for (i = 0; i < 49; i++)
 	{
 		printf("%d, ", fib[i]);
 	}
