b4af542e3f8101e7cf0c579f929b3cd40d7de3a8
Titus Njiru <titokym96@gmail.com>
2023-08-02 09:30:30 -0700

    add fib program

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the sum of even-valued terms in the Fibonacci sequence up to 4,000,000.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long fib[3] = {1, 2, 0};
+	unsigned long sum = 2;
+
+	while (fib[2] <= 4000000)
+	{
+		fib[2] = fib[0] + fib[1];
+		if (fib[2] % 2 == 0)
+			sum += fib[2];
+		fib[0] = fib[1];
+		fib[1] = fib[2];
+	}
+
+	printf("%lu\n", sum);
+	
+	return (0);
+}
+
