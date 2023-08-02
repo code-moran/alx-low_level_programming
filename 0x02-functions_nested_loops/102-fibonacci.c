4a6515664ad45a8d5a9418f448192466fedf4f63
Titus Njiru <titokym96@gmail.com>
2023-08-02 08:58:00 -0700

    betty style format

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints the first 50 Fibonacci numbers separated by commas and spaces.
+ * Description: Prints the first 50 Fibonacci numbers
  *
  * Return: Always 0 (Success)
  */
