c9bb3aab512f9083f707cf461fa60ae1ed6f44aa
Titus Njiru <titokym96@gmail.com>
2023-09-21 10:17:14 -0700

    fix comments

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,8 +6,7 @@ void first(void) __attribute__ ((constructor));
  * before_main - Function executed before the main function.
  *
  * Description: This function is executed before the main function starts
- *              and prints the message "You're beat! and yet, you must allow,
- *              \nI bore my house upon my back!\n".
+ *
  */
 void first(void)
 {
